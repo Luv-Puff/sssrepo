d36bb0063ca6e3e6d25c493914db2329127be2e3
kidneyweakx <kidneyweakx@gmail.com>
2020-04-09 19:48:29 +0800

    add tgbot func

--- main.go
+++ main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
+
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
+	godotenv.Load()
+	var (
+		port      = os.Getenv("PORT")       // sets automatically
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
 	crawl("https://acgn-stock.com/company/1")
 }
 
